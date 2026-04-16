f0a5b7a37bc11122238ec927db1908697540380e
aubindiscord03 <aubin.discord03@gmail.com>
2026-04-16 15:24:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+from playwright.sync_api import sync_playwright
+import os
+
+URL = os.getenv("PRONOTE_URL")
+USERNAME = os.getenv("PRONOTE_USER")
+PASSWORD = os.getenv("PRONOTE_PASS")
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            args=["--no-sandbox", "--disable-blink-features=AutomationControlled"]
+        )
+
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        )
+
+        page = context.new_page()
+
+        # anti-bot
+        page.add_init_script("""
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        })
+        """)
+
+        print("Ouverture PRONOTE...")
+        page.goto(URL)
+
+        page.wait_for_load_state("networkidle")
+        page.wait_for_timeout(5000)
+
 print("Page chargée")
 
 # 🔍 vérifier si déjà connecté
@@ -29,7 +62,7 @@ else:
 
     print("Connexion effectuée")
 
-browser.close()
+        browser.close()
 
 if __name__ == "__main__":
     main()
